340c3ea5b574f6ca001e3476a96de7201547c0f6
nshamee <nabilshamee@gmail.com>
2024-03-22 14:29:13 -0400

    Added features

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,8 @@ from ship import Ship
 
 from bullet import Bullet
 
+from alien import Alien
+
 class AlienInvasion:
 
     def __init__(self):
@@ -21,6 +23,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
 
@@ -67,6 +72,9 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+    def _create_fleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
@@ -74,6 +82,8 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+
         pygame.display.flip()
 
 if __name__=='__main__':
