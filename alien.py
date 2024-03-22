8484a85367a17261c19c264622e70713666dae70
nshamee <nabilshamee@gmail.com>
2024-03-22 12:23:48 -0400

    Added features

--- alien.py
+++ alien.py
@@ -1,1 +1,12 @@
 import pygame
+fromfrom pygame.sprite import Sprite
+
+class Alien(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.screen = ai_game.screen
+
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.rect.width
