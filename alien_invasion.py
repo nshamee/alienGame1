68f3654d2ac4558485d44ef4293ffd74c20c8735
nshamee <nabilshamee@gmail.com>
2024-03-23 21:18:30 -0400

    Added features, alien fleet, background

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,17 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+        collisions = pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
+
+    def _update_aliens(self):
+        self._check_fleet_edges()
+        self.aliens.update()
+
     def _create_fleet(self):
         alien = Alien(self)
         alien_width,alien_height = alien.rect.size
@@ -97,10 +108,19 @@ class AlienInvasion:
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
-
-
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
