77759f0ef0e49a2b98cb2ed234af1392b9762b81
nshamee <nabilshamee@gmail.com>
2024-03-30 21:11:55 -0400

    Added features, alien fleet, background

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,8 @@ from settings import Settings
 
 from game_stats import GameStats
 
+from button import Button
+
 from ship import Ship
 
 from bullet import Bullet
@@ -30,6 +32,7 @@ class AlienInvasion:
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
         self._create_fleet()
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
 
@@ -51,6 +54,16 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+    def _check_play_button(self,mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.reset_stats()
+            self.stats.game_active = True
+            self.aliens.empty()
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -153,6 +166,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         pygame.display.flip()
 
